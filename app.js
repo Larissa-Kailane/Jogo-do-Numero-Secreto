33b4f6cc939cf2220bae46add303e46b4e68ec30
Larissa Kailane <143636716+Larissa-Kailane@users.noreply.github.com>
2024-08-31 19:44:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 20;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+//função para exibir os textos na tela
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
+//mensagens principais
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1','Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 a 20');
 }
 
+//mensagem na tela
 exibirMensagemInicial();
 
+//verificando o chute com if e else
 function verificarChute(){
     let chute = document.querySelector('input').value;
 
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'ACERTOU!');
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        exibirTextoNaTela('h1', 'ACERTOU!'); //mensagem exibida para o acerto
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; //cálculo para as tentativas
 
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
-        exibirTextoNaTela('p', mensagemTentativas);
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;//mensagem para tentativas
+        exibirTextoNaTela('p', mensagemTentativas);//exibindo a msg das tentativas
 
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
-        if (chute > numeroSecreto) {
+        if (chute > numeroSecreto) { //exibindo as dicas
             exibirTextoNaTela('p', 'O número secreto é menor que o chute.');
         } else {
             exibirTextoNaTela('p', 'O número secreto é maior que o chute.');
         }
-        tentativas++;
+        tentativas++; //incrementando as tentativas
         limparCampo();
     }
 }
 
+//função para gerar um número aleatório novo, a cada vez que o jogo for iniciado
 function gerarNumeroAleatorio() {
     let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
@@ -54,11 +59,13 @@ function gerarNumeroAleatorio() {
     }
 }
 
+//limpando o campo do chute
 function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
     
 }
+//reiniciando o jogo, chamando a função de gerar um novo número para adivinhar e recomeçando as tentativas
 function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
